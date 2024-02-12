7cc734e61e1d7133a68661521d4a8d6d7632b7f5
shibil <shbl.salim@gmail.com>
2024-02-12 19:56:43 +0530

    Enhanced UI layout, styling, and integrated dummy data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,12 +1,55 @@
-import { View } from "react-native";
+import { View ,StyleSheet} from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of balls ',
+        amount : 45,
+        date: new Date('2023-11-06')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of socks ',
+        amount : 75,
+        date: new Date('2022-11-06')
+    },
+    {
+        id: 'e3',
+        description: 'A pair of hooks ',
+        amount : 65,
+        date: new Date('2023-02-23')
+    },
+    {
+        id: 'e4',
+        description: 'A bucket ',
+        amount : 98,
+        date: new Date('2020-01-26')
+    },
+    {
+        id: 'e5',
+        description: 'A Coffe Mug',
+        amount : 750,
+        date: new Date('2026-10-06')
+    },
+];
 
 function ExpensesOutput({expenses ,expensesPeriod}){
-    return <View>
-        <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
-        <ExpensesList/>
+    return(
+         <View style={styles.container }>
+        <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
+        <ExpensesList expenses={DUMMY_EXPENSES}/>
     </View>
-
+ );
 }
 export default ExpensesOutput;
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        padding:24,
+        backgroundColor: GlobalStyles.colors.primary700
+    }
+});
