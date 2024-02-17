1d3cf4a7619745c372337cbe694b391e2000303d
shibil <shbl.salim@gmail.com>
2024-02-17 23:55:23 +0530

    Implemented expense management screen with robust functionality

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,9 +1,16 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense({route}) {
+function ManageExpense({route,navigation}) {
 
 const editedExpenseId = route.params?.expenseId;
 const isEditing =  !!editedExpenseId;
+
+useLayoutEffect(()=>{
+navigation.setOptions({
+    title: isEditing ? 'Edit Expense' : 'Add Expense'
+});
+},[navigation, isEditing]);
 
 return <Text> ManageExpenses Screen</Text>;
 }
